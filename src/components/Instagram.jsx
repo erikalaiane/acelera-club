c6b70de7fcfa0b7786fd5778b839c7d0e1aef748
Erika Laiane <erikalaianeazevedosantos@gmail.com>
2025-11-24 11:34:49 -0300

    images

--- src/components/Instagram.jsx
+++ src/components/Instagram.jsx
@@ -1,15 +1,14 @@
 import React from 'react';
+import img1 from 'public/images/instagram/1.jpg';
+import img2 from 'public/images/instagram/2.jpg';
+import img3 from 'public/images/instagram/3.jpg';
+import img4 from 'public/images/instagram/4.jpg';
+import img5 from 'public/images/instagram/5.jpg';
+import img6 from 'public/images/instagram/6.jpg';
 import { Instagram as InstagramIcon } from 'lucide-react';
 
 const Instagram = () => {
-  const images = [
-    'images/instagram/1.jpg',
-    'images/instagram/2.jpg',
-    'images/instagram/3.jpg',
-    'images/instagram/4.jpg',
-    'images/instagram/5.jpg',
-    'images/instagram/6.jpg',
-  ];
+  const images = [img1, img2, img3, img4, img5, img6];
 
   return (
     <section className="py-24 bg-gray-900 relative overflow-hidden">
